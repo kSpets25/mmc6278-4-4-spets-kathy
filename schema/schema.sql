3ad0132efc8919bd78fec1223336bd1fe467956e
kSpets25 <142757766+kSpets25@users.noreply.github.com>
2024-06-05 17:42:30 -0500

    updated new unsolved files for assignment

--- schema/schema.sql
+++ schema/schema.sql
@@ -5,10 +5,8 @@ USE music_db;
 CREATE TABLE artists (
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 -- create a "name" column that's a varchar of max 100 characters
-  name VARCHAR(100) NOT NUll
-
+   name VARCHAR(100) NOT NUll
 );
-
 CREATE TABLE albums (
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 -- create a "name" column that's a varchar of max 100 characters
@@ -16,7 +14,6 @@ CREATE TABLE albums (
 -- create an "artist_id" column that's an integer
   artist_id INT, NOT NULL,
 -- create a "release_year" column that's an integer
-  release_year INT,
   FOREIGN KEY (artist_id)
     REFERENCES artists (id)
     ON DELETE CASCADE
@@ -27,9 +24,9 @@ CREATE TABLE songs (
 -- create a "name" column that's a varchar of max 100 characters
   name VARCHAR (100),
 -- create a "track_number" column that's an integer
-  track_number INT, 
+  artist_id INT,
 -- create an "artist_id" column that's an integer
-  artist_id INT, 
+  artist_id INT,
 -- create an "album_id" column that's an integer
   albuum_id INT,
   FOREIGN KEY (artist_id)
