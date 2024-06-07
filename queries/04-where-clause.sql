0c82fabc5b8bfea059d649dc7b18bfb4e5d399d0
kSpets25 <142757766+kSpets25@users.noreply.github.com>
2024-06-07 16:51:43 -0500

    changed queries where and insert code to pass tests

--- queries/04-where-clause.sql
+++ queries/04-where-clause.sql
@@ -1,4 +1,3 @@
 -- Write a query that selects all albums made after 2010
-SELECT albums.name, release_year 
-    FROM albums 
-    WHERE release_year > 2010
+SELECT * FROM albums
+WHERE release_year > 2010;
--- queries/06-insert.sql
+++ queries/06-insert.sql
@@ -1,5 +1,6 @@
 -- Add Track #13 titled "Endless, Nameless" to Nirvana's Nevermind album
 -- Nirvana's id is 6, Nevermind's id is 9
-INSERT INTO songs (artist_id, album_id, song_name, track_number)
-VALUES (6,9,"Endless Nameless", 13)
+
+INSERT INTO songs (artist_id, album_id, name, track_number)
+VALUES (6, 9, "Endless, Nameless", 13)
 
